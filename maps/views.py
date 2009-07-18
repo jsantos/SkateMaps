2691cac55ea6a340e091da060b4baf5fc1d527f4
Jorge Oliveira Santos <jorgeoliveirasantos@gmail.com>
2009-07-19 00:20:19 +0100

    Integration with SAPO Mapas®

--- maps/views.py
+++ maps/views.py
@@ -15,6 +15,10 @@ def spotdetail(request, spot_id):
 	spot = get_object_or_404(Spot, pk=spot_id)
 	return render_to_response('spotdetail.html', {'spot': spot, 'user': request.user})
 
+def spotmap(request, spot_id):
+	spot = get_object_or_404(Spot, pk=spot_id)
+	return render_to_response('spotmap.html', {'spot': spot, 'user': request.user})
+	
 def logout_page(request):
 	logout(request)
 	return HttpResponseRedirect('/')		
